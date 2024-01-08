f4b28926449dbdf17729a4a2e6071665f4526f1e
Skyenought <skyenought@qq.com>
2024-01-08 12:52:46 +0800

    initial commit

--- agent/agent.thrift
+++ agent/agent.thrift
@@ -1,10 +1,13 @@
 namespace go agent
 
 include "../model/task.thrift"
+include "../model/replace_task.thrift"
 
 struct UpdateTasksReq{
     1: list<task.Task> tasks
 }
+
+
 struct UpdateTasksRes{
     1: i32 code
     2: string msg
